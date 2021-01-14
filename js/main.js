6e38d3f4ac95e08c6f23e3264b4f7f7d5b0fffd8
vanessasilva-cs <vanesilva.cs@gmail.com>
2021-01-14 11:50:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,40 +12,17 @@
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */ 
-setTimeout(function() {
-  fadeOutPreloader(document.getElementById('preloader'), 69);
-}, 1500);
+$(window).load(function() {
 
-$(document).ready(function() {
-  $(window).on('beforeunload', function() {
-    window.scrollTo(0, 0);
-  });
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
 
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
 
-  function fadeOutPreloader(element, duration) {
-  opacity = 1;
+      });       
 
-  interval = setInterval(function() {
-    if (opacity <= 0) {
-      element.style.zIndex = 0;
-      element.style.opacity = 0;
-      element.style.filter = 'alpha(opacity = 0)';
-
-      // Allow horizontal scroll
-      document.documentElement.style.overflowY = 'auto';
-
-      // Remove preloader div
-      document.getElementById('preloader').remove();
-
-      clearInterval(interval);
-    } else {
-      opacity -= 0.1;
-      element.style.opacity = opacity;
-      element.style.filter = 'alpha(opacity = ' + opacity * 100 + ')';
-    }
-  }, duration);
-}
-
+  	})
 
   	/*---------------------------------------------------- */
   	/* FitText Settings
